62396c7c32bcb08cb85e877e26ad65d668fdcb8b
fool <habitdev@163.com>
2024-06-26 16:29:44 +0800

    first commit

---
+++ src/main/java/top/wmd001/LearnSpringShellApplication.java
@@ -0,0 +1,13 @@
+package top.wmd001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringShellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringShellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/wmd001/LearnSpringShellApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wmd001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringShellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
